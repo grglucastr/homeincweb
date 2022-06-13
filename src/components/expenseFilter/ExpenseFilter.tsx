b8bf445d0eb94598f43044e4e8aaf828021f6f5e
= <george.bentes@gmail.com>
2022-06-13 14:34:40 -0300

    Import React module

--- src/components/expenseFilter/ExpenseFilter.tsx
+++ src/components/expenseFilter/ExpenseFilter.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Row, Col, Button } from "react-bootstrap";
 import IExpenseFilter from "../../models/IExpenseFilter";
 
