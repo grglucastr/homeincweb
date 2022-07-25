19c2f9136bfd418356cb95d5ed146ccf74559b6c
= <george.bentes@gmail.com>
2022-07-25 15:12:18 -0300

    Add env variables for backend apis

--- src/http-commons.ts
+++ src/http-commons.ts
@@ -1,6 +1,11 @@
 import axios from 'axios';
+
+const ENV_BASE_URL = process.env.REACT_APP_HOME_API_URL || 'http://localhost:8080/v2';
+
+console.log("var value", ENV_BASE_URL);
+
 export default axios.create({
-    baseURL: 'http://localhost:8080/v2',
+    baseURL: ENV_BASE_URL,
     headers: {
         "Content-Type": "application/json"
     }
