8aa392536a40014c70ce4bbb250cb2f202aaa346
= <george.bentes@gmail.com>
2022-09-16 08:28:31 -0300

    [BAD PRACTICE] Fixed prod IP address

--- Dockerfile
+++ Dockerfile
@@ -6,9 +6,6 @@ WORKDIR /usr/src/app
 
 ENV PATH /app/node_modules/.bin:$PATH
 
-ARG HOMEINCAPI_URL
-ENV REACT_APP_HOME_API_URL=$HOMEINCAPI_URL
-
 RUN npm install --silent
 
 RUN npm install react-scripts -g --silent
--- src/http-commons.ts
+++ src/http-commons.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const ENV_BASE_URL = process.env.NODE_ENV === "production" ? process.env.REACT_APP_HOME_API_URL : "http://local.com";
+const prod_url = "http://172.17.0.3:8080/v2/";
+const ENV_BASE_URL = process.env.NODE_ENV === "production" ? prod_url : "http://local.com";
 
 console.log("Node Environment: ",process.env.NODE_ENV);
 console.log("var value", ENV_BASE_URL);
